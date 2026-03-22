0c8d8fd30b8d858b884279a752bee65e8f9db85d
Daniel Rudolf <github.com@daniel-rudolf.de>
2026-03-22 03:12:15 +0100

    Add Python-based `xmllint` polyfill to `latest-version.sh`

--- latest-version.sh
+++ latest-version.sh
@@ -22,7 +22,7 @@ export LC_ALL=C.UTF-8
 [ -x "$(type -P curl 2>/dev/null)" ] \
     || { echo "Missing script dependency: curl" >&2; exit 1; }
 
-[ -x "$(type -P xmllint 2>/dev/null)" ] \
+[ -x "$(type -P xmllint 2>/dev/null)" ] || [ -x "$(type -P python3 2>/dev/null)" ] \
     || { echo "Missing script dependency: xmllint" >&2; exit 1; }
 
 source "$CI_TOOLS_PATH/helper/common.sh.inc"
@@ -32,14 +32,30 @@ source "$BUILD_DIR/container.env"
 
 VERSION_REGEX="\([0-9]\+\.[0-9]\+\.[0-9]\++[0-9]\{6\}\)"
 
+_xpath_url() {
+    local URL_PREFIX="$1"
+    if [ -x "$(type -P xmllint 2>/dev/null)" ]; then
+        echo + "xmllint --html --xpath $(quote "//a[starts-with(@href, '$URL_PREFIX')]/@href") -" \
+            "| awk 'match(\$0, /^\s*href=\"([^\"]+)\"\s*/, m) { print m[1] }'" >&2
+        xmllint --html --xpath "//a[starts-with(@href, '$URL_PREFIX')]/@href" - 2> /dev/null \
+            | awk 'match($0, /^\s*href="([^"]+)"\s*/, m) { print m[1] }'
+    else
+        echo + "python3 -c <PYTHON_XPATH_PROGRAM> $(quote "$URL_PREFIX")" >&2
+        python3 -c "$(printf '%s\n' \
+            "import sys" \
+            "from html.parser import HTMLParser" \
+            "HTMLParser.handle_starttag=lambda _, tag, attrs: \\" \
+            "    [ print(value) for attr, value in attrs if tag == 'a' and attr == 'href' and value and value.startswith(sys.argv[1]) ]" \
+            "HTMLParser().feed(sys.stdin.read())" \
+        )" "$URL_PREFIX"
+    fi
+}
+
 echo + "curl -sSL $(quote "$DOWNLOADS_WEBPAGE_URL")" >&2
 DOWNLOADS_WEBPAGE="$(_curl "$DOWNLOADS_WEBPAGE_URL" | sed -e '1,/^$/d')"
 [ -n "$DOWNLOADS_WEBPAGE" ] || { echo "Failed to request LimeSurvey downloads webpage: $DOWNLOADS_WEBPAGE_URL" >&2; exit 1; }
 
-echo + "xmllint --html --xpath $(quote "//a[starts-with(@href, '${ARCHIVE_URL_TEMPLATE%/*}')]/@href") -" \
-    "| awk 'match(\$0, /^\s*href=\"([^\"]+)\"\s*/, m) { print m[1] }'" >&2
-ARCHIVE_URL="$(xmllint --html --xpath "//a[starts-with(@href, '${ARCHIVE_URL_TEMPLATE%/*}')]/@href" - 2> /dev/null <<< "$DOWNLOADS_WEBPAGE" \
-    | awk 'match($0, /^\s*href="([^"]+)"\s*/, m) { print m[1] }')"
+ARCHIVE_URL="$(_xpath_url "${ARCHIVE_URL_TEMPLATE%/*}" <<< "$DOWNLOADS_WEBPAGE")"
 [ -n "$ARCHIVE_URL" ] || { echo "Malformed LimeSurvey downloads webpage: $DOWNLOADS_WEBPAGE_URL" >&2; exit 1; }
 
 ARCHIVE_URL_REGEX="$(printf "$(sed -e 's/[]\/$*.^[]/\\&/g' <<< "$ARCHIVE_URL_TEMPLATE")" "$VERSION_REGEX")"
